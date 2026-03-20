fa2831b422b6b0a8d92a16f06839c0ebc50ef511
Divya Dharshini <divya.tvr.2006@gmail.com>
2026-03-20 08:28:22 +0530

    secure

--- app.py
+++ app.py
@@ -1,26 +1,21 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import os
 from handler import process_image
-
 app = Flask(__name__)
-
+CORS(app)
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
 @app.route('/upload', methods=['POST'])
 def upload_image():
     try:
         file = request.files['image']
-
         file_path = os.path.join(UPLOAD_FOLDER, file.filename)
         file.save(file_path)
-
         result = process_image(file_path)
-
         return jsonify(result)
-
     except Exception as e:
         return jsonify({"status": "ERROR", "message": str(e)})
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+   
